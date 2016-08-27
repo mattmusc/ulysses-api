dab484cec0c41e9bd9dfd801e1bca7eba1d24752
Matteo Muscella <matteo.muscella@usi.ch>
2016-08-27 15:55:14 +0200

    Correct JSON response on notesIndex request
    Add code to send back Json with fake notes

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Note struct {
+	Title     string  `json:"title"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Info      string  `json:"info"`
+	Audio     string  `json:"audio"`
+}
+type Notes []Note
+
+// Global variables
+var notes Notes
+
 func main() {
-	println("hello!")
+	DEBUG := true
+
+	if DEBUG {
+		notes = Notes{Note{Title: "Paris", Latitude: 5.498, Longitude: 6.689, Info: "Gretest city", Audio: "01.mp3"}}
+	}
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/notes", notesIndex)
+
+	http.ListenAndServe(":9000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func notesIndex(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewEncoder(w).Encode(notes); err != nil {
+		panic(err)
+	}
 }
