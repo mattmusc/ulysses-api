15f2207c6ceb02d1280a33a0297b9b850633a57f
Matteo Muscella <matteo.muscella@usi.ch>
2016-08-28 15:52:48 +0200

    Two audio notes
    Add two audio notes

--- main.go
+++ main.go
@@ -31,8 +31,8 @@ func main() {
 
 	if DEBUG {
 		notes = Notes{
-			Note{Title: "Spazio Officina", Latitude: 45.837, Longitude: 9.027, City: "Chiasso", Address: "Svizzera, Via D. Alighieri 4, 6830 Chiasso", Audio: "chiasso_01.mp3", Tagger: "Elena"}}
-		//Note{Title: "M.A.X. Museo", Latitude: 45.80, Longitude: 9.090, City: "Chiasso", Address: "Svizzera, Via D. Alighieri 6, 6830 Chiasso", Audio: "chiasso_02.m4a", Tagger: "Andrea"}}
+			Note{Title: "Spazio Officina", Latitude: 45.837, Longitude: 9.027, City: "Chiasso", Address: "Svizzera, Via D. Alighieri 4, 6830 Chiasso", Audio: "chiasso_01.m4a", Tagger: "Elena"},
+			Note{Title: "M.A.X. Museo", Latitude: 45.80, Longitude: 9.090, City: "Chiasso", Address: "Svizzera, Via D. Alighieri 6, 6830 Chiasso", Audio: "chiasso_02.m4a", Tagger: "Andrea"}}
 	}
 
 	r := mux.NewRouter()
